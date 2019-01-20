09cf0eb77459a77744f11a31f386902dd8e2e8f1
Zhan Li <zhan.li.dev@outlook.com>
2019-01-20 20:49:13 +1100

    Updated lazy singletion comment.

--- src/com/zhandev/singleton/LazySingleObject.java
+++ src/com/zhandev/singleton/LazySingleObject.java
@@ -14,7 +14,7 @@ public class LazySingleObject {
 	// NOTE "synchronized" here, you must add it
 	// otherwise, it is not thread safe
 	// as when multiple threads invoke this method, there will be something wrong, although the result may look right
-	// conclusion: do not recommend to write in this way due to performance problem
+	// conclusion: DO NOT RECOMMEND writing in this way due to performance problem
 	public static synchronized LazySingleObject getInstance() {
 		if (lazyInstance == null) {
 			lazyInstance = new LazySingleObject();
